cf58bb18d74677480d836d442e99e04acc4450d5
Kryštof Svejkovský <svejkovsky.kry@gmail.com>
2023-04-25 15:42:09 +0200

    added simple water

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import Earth from './Earth';
 import Moon from './Moon';
+import Water from './Water';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Water />
       <Earth />
       <Moon />
     </div>
---
+++ src/Water.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Water.css';
+
+const Water = () => {
+  return <div className="water" aria-label="Water"></div>;
+};
+
+export default Water;
