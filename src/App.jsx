02a17518e20e9f66c2af8ac7fd13f12eb21adf60
Kryštof Svejkovský <svejkovsky.kry@gmail.com>
2023-04-25 13:31:56 +0200

    created earth and moon

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Earth from './Earth';
+import Moon from './Moon';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Earth />
+      <Moon />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Earth.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Earth.css';
+
+const Earth = () => {
+  return <div className="earth" aria-label="Earth"></div>;
+};
+
+export default Earth;
